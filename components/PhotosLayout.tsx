758b5c797a64087a24748d9a7d403a46edad7ff8
chikwesi <kwesitor@gmail.com>
2023-02-04 17:06:13 +0100

    set up photo to use up next imaage component

--- components/PhotosLayout.tsx
+++ components/PhotosLayout.tsx
@@ -1,16 +1,16 @@
+import Image from 'next/image'
 import styled from 'styled-components'
 
-
 export const PhotoGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 0.5rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 0.5rem;
 `
 
-export const Photo = ({ url }) => {
+export const Photo = ({ url, altText }) => {
   return (
     <Wrapper>
-      <img src={url} />
+      <Image src={url} alt={altText} width={300} height={300} />
     </Wrapper>
   )
 }
@@ -22,7 +22,7 @@ const Wrapper = styled.div`
   border: 1px solid red;
   line-height: 0;
 
-  img{
+  img {
     width: 100%;
     height: auto;
   }
--- next.config.js
+++ next.config.js
@@ -2,5 +2,15 @@ module.exports = {
     compiler: {
         // Enables the styled-components SWC transform
         styledComponents: true
-    }
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/djnlxla7q/**',
+          },
+        ],
+      },
 }
